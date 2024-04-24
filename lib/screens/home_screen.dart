35bb814c3eadd1b62e616a31cf7375a9ef2d462b
Ene Matei <ene.matei21@gmail.com>
2024-04-24 14:47:17 +0300

    added reorderable list view

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<int> _items = [];
+
   void _newItemButtonPressed(BuildContext context) {
     showDialog(
       context: context,
@@ -19,6 +21,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -26,7 +32,27 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         centerTitle: true,
       ),
-      body: Container(),
+      body: ReorderableListView(
+        children: <Widget>[
+          for (int i = 0; i < _items.length; i++)
+            ListTile(
+              key: Key('$i'),
+              tileColor: _items[i].isOdd ? oddItemColor : evenItemColor,
+              title: Text("Item ${_items[i]}"),
+              trailing: const Icon(Icons.drag_handle_sharp),
+            ),
+        ],
+        onReorder: (int oldIndex, int newIndex) {
+          setState(() {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+
+            final int item = _items.removeAt(oldIndex);
+            _items.insert(newIndex, item);
+          });
+        },
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           _newItemButtonPressed(context);
